df9d844783e5cae9772889a32f737a5924a1fd03
Cesar Rodriguez <kesarr@gmail.com>
2018-11-02 01:13:48 +0100

    init

---
+++ src/server.ts
@@ -0,0 +1,45 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load();
+}
+
+console.log('starting...');
+
+const cron = require('node-cron');
+const axios = require('axios');
+const Eos = require('eosjs');
+
+const eosJS = Eos({
+  keyProvider: process.env.PRIVATE_KEY,
+  httpEndpoint: process.env.ENDPOINT,
+  chainId: process.env.CHAIN_ID
+});
+
+cron.schedule('* * * * *', () => {
+  axios
+    .get('https://api.bitfinex.com/v2/ticker/' + process.env.PAIR)
+    .then((response: any) => {
+      const pair = parseFloat(response.data[6]).toFixed(4);
+      console.log(pair);
+      eosJS.transaction({
+        actions: [
+          {
+            account: process.env.NEBULA_ACCOUNT,
+            authorization: [
+              { actor: process.env.ACCOUNT, permission: process.env.PERMISSION }
+            ],
+            data: {
+              account: process.env.ACCOUNT,
+              base: process.env.BASE,
+              quote: pair + ' ' + process.env.QUOTE,
+              exchange: process.env.EXCHANGE,
+              volume: 0,
+              timeframe: 60
+            },
+            name: 'addquote'
+          }
+        ]
+      }).catch((error: any) => {
+        console.error(error);
+      })
+    });
+});
